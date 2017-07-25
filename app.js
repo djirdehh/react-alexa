55c9e5461e2050a1b4c608b7efbc022413a83404
djirdehh <hassan.djirdeh@mail.utoronto.ca>
2017-07-24 22:58:28 -0400

    initial scaffold

---
+++ app.js
@@ -0,0 +1,9 @@
+import React, { Component, PropTypes} from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    <h1>React - Alexa</h1>
+  </div>,
+  document.getElementById('app')
+);
