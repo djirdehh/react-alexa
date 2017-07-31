98b6cd84f03f53ff696b059a2717ffe53a9ae129
djirdehh <hassan.djirdeh@mail.utoronto.ca>
2017-07-30 21:44:36 -0400

    line restructuring

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const http = require("http");
 const express = require('express');
+
 const app = express();
+const server = http.createServer(app);
 
 app.use('/public', express.static('./public'));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 })
-
-const server = http.createServer(app);
 
 server.listen(3000, () => {
 	console.log('listening on Port 3000!');
