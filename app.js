349ccf6f251bc10028f82d5fc4bc042a024a0758
djirdehh <hassan.djirdeh@mail.utoronto.ca>
2017-07-30 21:18:05 -0400

    Slight configure to scaffold

--- app.js
+++
@@ -1,9 +0,0 @@
-import React, { Component, PropTypes} from 'react';
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(
-  <div>
-    <h1>React - Alexa</h1>
-  </div>,
-  document.getElementById('app')
-);
